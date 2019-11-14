74e4ac4ff17a1cb847760889faff9129375cd735
Stefano Longhi <stefano@moneyfarm.com>
2019-11-14 22:34:39 +0000

    cleaned Mission class

--- Context/InTheTavern/Mission.cs
+++ Context/InTheTavern/Mission.cs
@@ -4,15 +4,10 @@ namespace HRSaga.Context.InTheTavern
 {
     public class Mission : ValueObject
     {
-        private int reward = 10;
 
         public Mission()
         {
         }
 
-        public int GetReward()
-        {
-            return reward;
-        }
     }
 }
